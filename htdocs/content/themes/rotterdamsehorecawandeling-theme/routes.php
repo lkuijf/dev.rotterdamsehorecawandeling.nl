8c4c109982dc16c65a1f790d653bbfc0040fef8f
Leon Kuijf <leon.kuijf@gmail.com>
2024-01-09 10:57:29 +0100

    added page- and postcontroller + routes to global application routes

--- htdocs/content/themes/rotterdamsehorecawandeling-theme/routes.php
+++ htdocs/content/themes/rotterdamsehorecawandeling-theme/routes.php
@@ -1,39 +1,7 @@
 <?php
-
 /**
  * Theme routes.
  *
  * The routes defined inside your theme override any similar routes
  * defined on the application global scope.
  */
-Route::any('/', function () {
-    return view('pages.front');
-});
-Route::any('page', function () {
-    return view('pages.default');
-});
-Route::any('single', function () {
-    return view('blog.single');
-});
-Route::any('archive', function () {
-    return view('blog.archive');
-});
-Route::any('category', function () {
-    return view('blog.archive');
-});
-Route::any('tag', function () {
-    return view('blog.archive');
-});
-Route::any('search', function () {
-    return view('pages.search');
-});
-
-
-
-
-// Route::any('404', function () {
-//     return view('pages.default');
-// });
-Route::fallback(function () {
-    return view('errors.404');
-});
